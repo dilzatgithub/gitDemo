92063b3a362fc8c5b76b908bb0af66fb0fea26d5
dilzatgithub <71199942+dilzatgithub@users.noreply.github.com>
2020-09-17 20:28:12 -0700

    Update Hello2020.java

--- src/Hello2020.java
+++ src/Hello2020.java
@@ -2,5 +2,6 @@ public class Hello2020 {
     public static void main(String[] args) {
         System.out.println("Hello my guy");
         System.out.println("jahahahah");
+        // this linr is added
     }
 }
